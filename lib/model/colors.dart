e4ededc752346d0c64b0ce4bfd26b13274c33ed9
Syed Shayan <3syedshayan@gmail.com>
2025-04-28 22:49:10 +0500

    Colors Consts added

---
+++ lib/model/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const Color kBackgroundColor = Color(0xFF1C1C1C);
+const Color kDisplayTextColor = Colors.white;
+const Color kHeaderColor = Color(0xFFE74C3C);
+const Color kOperationColor = Color(0xFFFF9500);
+const Color kEqualButtonColor = Color(0xFF4CAF50);
+const Color kClearButtonColor = Color(0xFFE74C3C);
+const Color kNumberButtonColor = Color(0xFF333333);
+const Color kExtraButtonColor = Color(0xFF505050);
